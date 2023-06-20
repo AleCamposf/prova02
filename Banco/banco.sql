15ca3b6bed5a0fd7ec2991cc0295925343c05cfc
AleCamposf <alexander.campos@aluno.ifsp.edu.br>
2023-06-20 12:11:45 -0300

    Prova

--- Banco/banco.sql
+++ Banco/banco.sql
@@ -1,10 +1,13 @@
-create database Prova2;
-drop table IF exists fluxo_caixa;
-create table fluxo_caixa (
-	id INTEGER not null AUTO_INCREMENT,
-    data DATE not null,
-    tipo VARCHAR(10) not null,
-    valor DECIMAL(10,2) not null,
-    historico VARCHAR(150) not null,
-    cheque VARCHAR(3) not null,
-    PRIMARY KEY (id) );
+drop database if exists prova2;
+create database prova2;
+use prova2;
+drop table if exists fluxo_caixa;
+create table fluxo_caixa(
+id integer not null auto_increment,
+data date not null,
+tipo varchar(10) not null,
+valor decimal(10,2) not null,
+historico varchar(150) not null,
+cheque varchar(3),
+primary key (id)
+);
